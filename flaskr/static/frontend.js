6ace2eb3acbb68c6a725c453b76bd2a617763bf8
Pierre <pierre.lefevre@elev.it-gymnasiet.se>
2019-02-20 16:34:06 +0100

    minor changes to appearance, and polish to the ux

--- flaskr/static/frontend.js
+++ flaskr/static/frontend.js
@@ -1,6 +1,7 @@
-$(window).on("load", function(){
-	
-});
+Date.prototype.getWeek = function(){
+					        var onejan = new Date(this.getFullYear(), 0, 1);
+					        return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+					     } 
 
 function showHideComments(element){
 	var toggle = $(element).parent().parent().find('.comments-box').fadeToggle('fast');
