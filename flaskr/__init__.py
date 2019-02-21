756f8ef80e9a521749d1cdc6962c63372017b240
Pierre <pierre.lefevre@elev.it-gymnasiet.se>
2019-02-21 14:13:42 +0100

    Hej

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,17 +4,14 @@ import re
 from jinja2 import evalcontextfilter, Markup, escape
 from flask import Flask
 
-def create_app(host, post, test_config=None):
+def create_app(host, port, test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
         DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
-    print(host)
-    print(" ++++ " + str(port))
-
-    #app.config['SERVER_HOST'] = host + ":" + port
+    app.config['SERVER_NAME'] = host['SERVER_NAME'] + ":" + host['SERVER_PORT']
 
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
