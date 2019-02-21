5f1ad21d75dba7d820127ad7a1f40603eefecf62
Pierre <pierre.lefevre@elev.it-gymnasiet.se>
2019-02-21 12:49:56 +0100

    added procfile

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -10,7 +10,6 @@ def create_app(test_config=None):
         SECRET_KEY="dev",
         DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
-    app.config['SERVER_NAME'] = "0.0.0.0"
 
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
