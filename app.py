49b066be5e152aa38242714a1404e68100334d62
iremiseton <harry.laza@outlook.com>
2019-03-20 12:22:40 +0100

    .

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = create_app()
 
 if __name__ == "__main__":
 	try:
-    	app.run(host="0.0.0.0", port=80, ssl_context=('cert.pem', 'key.pem'))
+		app.run(host="0.0.0.0", port=80, ssl_context=('cert.pem', 'key.pem'))
     except:
     	app.run(host="0.0.0.0", port=80)
