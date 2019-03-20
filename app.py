da0bd876a468eb8b70277da3b841fd4b2e6795b8
iremiseton <harry.laza@outlook.com>
2019-03-20 12:26:19 +0100

    .

--- app.py
+++ app.py
@@ -3,7 +3,4 @@ from flaskr import create_app
 app = create_app()
 
 if __name__ == "__main__":
-	try:
-		app.run(host="0.0.0.0", port=80, ssl_context=('cert.pem', 'key.pem'))
-	except:
-		app.run(host="0.0.0.0", port=80)
+	app.run(host="0.0.0.0", port=80)
